a6cd3a0cf8bfb76d13fc63986979184b23911ca1
magdavanda <magdakalder@gmail.com>
2026-08-22 23:52:38 +0200

    ex0 done

---
+++ battle.py
@@ -0,0 +1,42 @@
+import ex0
+
+def factory_check(factory: ex0.CreatureFactory) -> None:
+    base_creature = factory.create_base()
+    print(base_creature.describe())
+    print(base_creature.attack())
+
+    evolved_creature = factory.create_evolved()
+    print(evolved_creature.describe())
+    print(evolved_creature.attack())
+
+def base_fight(flame_factory: ex0.FlameFactory, aqua_factory: ex0.AquaFactory) -> None:
+    base_flame = flame_factory.create_base()
+    base_aqua = aqua_factory.create_base()
+    print(base_flame.describe())
+    print(" vs.")
+    print(base_aqua.describe())
+    print(" fight!")
+    print(base_flame.attack())
+    print(base_aqua.attack())
+
+
+def main() -> None:
+    print("Testing factory")
+
+    factory_flame = ex0.FlameFactory()
+    factory_aqua = ex0.AquaFactory()
+
+    factory_check(factory_flame)
+    print("\nTesting factory")
+    factory_check(factory_aqua)
+    
+    print()
+    print("Testing battle")
+    base_fight(factory_flame, factory_aqua)
+
+    
+    
+
+
+if __name__ == "__main__":
+    main()
